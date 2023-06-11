dc10f67c5625a10b254ed9befcb3c2baa00db8c4
fourth <fourthe.hero@gmail.com>
2023-06-11 17:53:37 +0700

    Update index.vue

--- nuxt-basic/pages/index.vue
+++ nuxt-basic/pages/index.vue
@@ -1,36 +1,20 @@
 <template>
   <div>
-    <header>
-        <nav>
-            <h2>M&M</h2>
-            <NuxtLink to="/">Home</NuxtLink>
-            <NuxtLink to="/artists">Artists</NuxtLink>
-            <NuxtLink to="/addartist">Add Artist</NuxtLink>
-        </nav>
-    </header>
     <h1>หน้าแรกของเว็บไซต์</h1>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+    useHead({
+        title:"Home page",
+        meta:[
+            {name:"description",content:"รายการศิลปินที่ชื่นชอบ"}
+        ]
+    })
 </script>
-
-<style scoped>
-    nav{
-        margin: 10px auto;
-        padding: 10px 0;
-        display:flex;
-        justify-content: left;
-        align-items: center;
-        background: #0F0F0F;
-    }
-    nav a{
-        color: white;
-        text-decoration: none;
-        font-size: 18px;
-        padding: 10px;
+<style>
+    *{
+        margin: 0;
+        padding: 0;
     }
 </style>
