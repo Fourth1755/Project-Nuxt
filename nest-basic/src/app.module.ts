64f5c98c74dd4ced82c6b203edc722a5ae236cfa
fourth <fourthe.hero@gmail.com>
2023-06-12 16:08:34 +0700

    Update app.module.ts

--- nest-basic/src/app.module.ts
+++ nest-basic/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistsController } from './artists/artists.controller';
+import { ArtistsService } from './artists/artists.service';
+import { ArtistsModule } from './artists/artists.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [ArtistsModule],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController, ArtistsController],
+  providers: [AppService, ArtistsService],
 })
 export class AppModule {}
