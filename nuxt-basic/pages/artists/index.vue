c398fadafef907f0ecf21fcbe15335a71b2497e6
fourth <fourthe.hero@gmail.com>
2023-06-17 16:53:41 +0700

    Update index.vue

--- nuxt-basic/pages/artists/index.vue
+++ nuxt-basic/pages/artists/index.vue
@@ -2,8 +2,18 @@
       <div class="container">
         <div class="content-container">
           <h1>ศิลปินทั้งหมด</h1>
+          <table>
+          <thead>
+            <th>No.</th>
+            <th>Name</th>
+            <th>Country</th>
+          </thead>
+          <tbody>
+            <tr></tr>
+          </tbody>
+        </table>
         </div>
-  
+        
 </div>
 </template>
 
